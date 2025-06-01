2ca5e55de0f7c02444aee7bf9e60e6797af15a0d
Fábio Alves <fbio.alves095@gmail.com>
2025-06-01 00:52:51 -0300

    feat: security feature to prevent backend php files to be directly served in browser

--- backend/admins.php
+++ backend/admins.php
@@ -1,5 +1,10 @@
 <?php
 
+if (basename(__FILE__) == basename($_SERVER['SCRIPT_FILENAME'])) {
+    http_response_code(403);
+    exit('Acesso proibido.');
+}
+
 require_once 'db.php';
 
 function validarLoginAdmin($email, $senha) {
