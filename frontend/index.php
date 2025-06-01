b2e7a42c1551bf13cd78c77d80cf528e7eb707b0
Fábio Alves <fbio.alves095@gmail.com>
2025-06-01 01:20:40 -0300

    refactor: update main page style and elements positioning

--- frontend/index.php
+++ frontend/index.php
@@ -11,31 +11,21 @@
     <a class="button-wrap button-default" href="./login.php">Login Admin</a>
   </header>
   <body>
-    <section>
+    <section class="content-section">
       <div>
-        <h1>Title Section 1</h1>
+        <h1>Banner com cães</h1>
         <span
-          >Texto explicando o aplicativo. Texto explicando o aplicativo. Texto
-          explicando o aplicativo. Texto explicando o aplicativo. Texto
-          explicando o aplicativo. Texto explicando o aplicativo. Texto
-          explicando o aplicativo. Texto explicando o aplicativo. Texto
-          explicando o aplicativo. Texto explicando o aplicativo. Texto
-          explicando o aplicativo. Texto explicando o aplicativo.</span
+          >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus accumsan pretium purus, eget lacinia ex. Sed eu laoreet lectus. Aliquam aliquam velit massa, nec gravida tellus tincidunt eget. Donec eget nulla risus. Curabitur at libero eget ipsum commodo consequat. Phasellus lectus libero, ullamcorper nec condimentum in, suscipit interdum metus. Integer at ante at sapien vehicula consequat ut in purus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis cursus eros non dapibus. Etiam eget leo aliquam, tristique metus tincidunt, condimentum magna. In non purus ante. Proin vel condimentum enim.</span
         >
       </div>
-      <img src="assets/banner-dogs.jpg" alt="" />
+      <img class="banner-figures" src="assets/banner-dogs.jpg" alt="" />
     </section>
-    <section>
-      <img src="assets/banner-dogs.jpg" alt="" />
+    <section  class="content-section">
+      <img class="banner-figures" src="assets/banner-cats.jpg" alt="" />
       <div>
-        <h1>Title Section 1</h1>
+        <h1>Banner com gatos</h1>
         <span
-          >Texto explicando o aplicativo. Texto explicando o aplicativo. Texto
-          explicando o aplicativo. Texto explicando o aplicativo. Texto
-          explicando o aplicativo. Texto explicando o aplicativo. Texto
-          explicando o aplicativo. Texto explicando o aplicativo. Texto
-          explicando o aplicativo. Texto explicando o aplicativo. Texto
-          explicando o aplicativo. Texto explicando o aplicativo.</span
+          >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus accumsan pretium purus, eget lacinia ex. Sed eu laoreet lectus. Aliquam aliquam velit massa, nec gravida tellus tincidunt eget. Donec eget nulla risus. Curabitur at libero eget ipsum commodo consequat. Phasellus lectus libero, ullamcorper nec condimentum in, suscipit interdum metus. Integer at ante at sapien vehicula consequat ut in purus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla mattis cursus eros non dapibus. Etiam eget leo aliquam, tristique metus tincidunt, condimentum magna. In non purus ante. Proin vel condimentum enim.</span
         >
       </div>
     </section>
